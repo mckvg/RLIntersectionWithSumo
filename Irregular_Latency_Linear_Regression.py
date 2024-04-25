cdc09b850fe18594ca56c93624eac6181bde40db
mckvg <lifukang0125@163.com>
2024-04-25 17:16:10 +0800

    Time Latency

--- Irregular_Latency_Linear_Regression.py
+++ Irregular_Latency_Linear_Regression.py
@@ -1,7 +1,8 @@
 import numpy as np
 from Time_Data import cloud_SPAT_intervals, vehicle_posterior_intervals
-from Time_Data import vehicle_posterior_data, cloud_SPAT
-from Exchange_Data import datatime_to_moy_timestamp
+from Time_Data import vehicle_posterior_data, cloud_SPAT_data, cloud_SPAT_verified_data
+from Regular_Latency_BP_Network import inference_final
+from Exchange_Data import datatime_to_moy_timestamp, moy_timestamp_to_datetime
 import datetime
 
 
@@ -16,11 +17,18 @@ intercept = coefficients[1]
 print("斜率:", slope)
 print("截距:", intercept)
 
-vehicle_posterior_list = datatime_to_moy_timestamp(vehicle_posterior_data)
+# irregular latency数据集
+# vehicle_posterior_list = datatime_to_moy_timestamp(vehicle_posterior_data)
+# could_SPAT = cloud_SPAT_data
 
-print("Vehicle Posterior:", vehicle_posterior_list)
+# 结合 regular BP network的后验预测值
+vehicle_posterior_list = datatime_to_moy_timestamp(inference_final)
+cloud_SPAT = cloud_SPAT_verified_data
+
+print("Vehicle Posterior:", inference_final)
 print("Cloud        SPAT:", cloud_SPAT)
 
+# 逻辑有问题处*
 for i in range(1, len(vehicle_posterior_list)):
     interval_vehicle = abs((vehicle_posterior_list[i][0]-vehicle_posterior_list[i-1][0])*60*1000+
            abs((vehicle_posterior_list[i][1]-vehicle_posterior_list[i-1][1])))
@@ -33,4 +41,5 @@ for i in range(1, len(vehicle_posterior_list)):
         vehicle_posterior_list[i][0] = vehicle_current // (60*1000)
         vehicle_posterior_list[i][1] = vehicle_current % (60*1000)
 
-print("Vehicle   Revised:", vehicle_posterior_list)
+final_vehicle_data = moy_timestamp_to_datetime(vehicle_posterior_list, cloud_SPAT[0][0])
+print("Vehicle   Revised:", final_vehicle_data)
